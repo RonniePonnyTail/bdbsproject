8c2acbe71c59d1db862801f292a2bf78bb8f9605
RonniePonnyTail <36692377+RonniePonnyTail@users.noreply.github.com>
2018-03-06 17:54:45 +0100

    form

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+ 
+ 
+function Connect()
+{
+ $dbhost = "den1.mysql2.gear.host:3306";
+ $dbuser = "musiclibrarydb";
+ $dbpass = "Va28URjY~Q6~";
+ $dbname = "MUSIC_LIBRARY_DB";
+ 
+ // Create connection
+ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
+ 
+ return $conn;
+}
+ 
+?>
---
+++ thankyou.php
@@ -0,0 +1,18 @@
+<?php
+ 
+require 'connection.php';
+$conn    = Connect();
+$jmeno   = $conn->real_escape_string($_POST['JMENO']);
+$prijmeni    = $conn->real_escape_string($_POST['PRIJMENI']);
+$email   = $conn->real_escape_string($_POST['LOGIN_EMAIL']);
+$heslo    = $conn->real_escape_string($_POST['HESLO']);
+$query   = "INSERT into tb_cform (JMENO, PRIJMENI, EMAIL, HESLO) VALUES('" . $jmeno . "','" . $prijmeni . "','" . $email . "','" . $heslo . "')";
+$success = $conn->query($query);
+ 
+if (!$success) {
+    die("Couldn't enter data: ".$conn->error);
+}
+ 
+$conn->close();
+ 
+?
