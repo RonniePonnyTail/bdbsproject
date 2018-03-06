bbdd9711749dc8d0cfa931634b91c51aab510470
RonniePonnyTail <36692377+RonniePonnyTail@users.noreply.github.com>
2018-03-06 18:23:46 +0100

    test

--- thankyou.php
+++ thankyou.php
@@ -6,9 +6,9 @@ $jmeno   = $conn->real_escape_string($_POST['JMENO']);
 $prijmeni    = $conn->real_escape_string($_POST['PRIJMENI']);
 $email   = $conn->real_escape_string($_POST['LOGIN_EMAIL']);
 $heslo    = $conn->real_escape_string($_POST['HESLO']);
-$query   = "INSERT into tb_cform (JMENO, PRIJMENI, EMAIL, HESLO) VALUES('" . $jmeno . "','" . $prijmeni . "','" . $email . "','" . $heslo . "')";
+$query   = "INSERT into uzivatel (JMENO, PRIJMENI, EMAIL, HESLO) VALUES('" . $jmeno . "','" . $prijmeni . "','" . $email . "','" . $heslo . "')";
 $success = $conn->query($query);
- 
+
 if (!$success) {
     die("Couldn't enter data: ".$conn->error);
 }
