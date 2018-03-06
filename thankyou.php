d0a42c5476456ff8c9cade091d06f4bef1a658db
RonniePonnyTail <36692377+RonniePonnyTail@users.noreply.github.com>
2018-03-06 18:25:02 +0100

    sdf

--- thankyou.php
+++ thankyou.php
@@ -6,7 +6,7 @@ $jmeno   = $conn->real_escape_string($_POST['JMENO']);
 $prijmeni    = $conn->real_escape_string($_POST['PRIJMENI']);
 $email   = $conn->real_escape_string($_POST['LOGIN_EMAIL']);
 $heslo    = $conn->real_escape_string($_POST['HESLO']);
-$query   = "INSERT into uzivatel (JMENO, PRIJMENI, EMAIL, HESLO) VALUES('" . $jmeno . "','" . $prijmeni . "','" . $email . "','" . $heslo . "')";
+$query   = "INSERT into uzivatel (JMENO, PRIJMENI, LOGIN_EMAIL, HESLO) VALUES('" . $jmeno . "','" . $prijmeni . "','" . $email . "','" . $heslo . "')";
 $success = $conn->query($query);
 
 if (!$success) {
