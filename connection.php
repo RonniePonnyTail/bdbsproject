11d61969f66cc6727c7c884cafb2fcfe9e561c31
RonniePonnyTail <36692377+RonniePonnyTail@users.noreply.github.com>
2018-03-06 18:20:29 +0100

    test

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ function Connect()
  $dbhost = "den1.mysql2.gear.host";
  $dbuser = "musiclibrarydb";
  $dbpass = "Va28URjY~Q6~";
- $dbname = "MUSIC_LIBRARY_DB";
+ $dbname = "musiclibrarydb";
  
  // Create connection
  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
